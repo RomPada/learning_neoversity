0fae22e022dacff0e0b85e011642cb13f83bba4d
Rom_work <work@email.com>
2025-10-13 11:05:21 +0300

    new_folder_"practice_neo"

---
+++ practice_neo/practice_sum_all_num.py
@@ -0,0 +1,12 @@
+num = int(input("Enter the integer (0 to 100): "))
+sum = 0
+veriable = 0
+
+while veriable < num:
+    veriable = veriable + 1
+    sum = sum + veriable
+
+
+print (num)
+print (sum)
+print (veriable)
