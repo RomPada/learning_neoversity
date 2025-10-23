7fb4edb3990ab375d94a5df66ed051d416642fa7
RomPadaя <reply033@gmail.com>
2025-10-23 22:41:14 +0300

    dayly_work

--- workspace.py
+++ workspace.py
@@ -1,12 +1,11 @@
-def format_string(string, length):
-    l = len(string)
-    if l >= length:
-        return(string)
-    else:
-        gap = (length - len(string)) // 2
-        string_gap = " " * gap
-        string = string_gap + string
-        return(string)
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a.intersection(b))  # {3}
 
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a.difference(b))  # {1, 2}
 
-        
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a.symmetric_difference(b))  # {1, 2, 4, 5}
